29b2fb3d726ddf515cb8b68987fe7ab7e167f8b8
boygl34 <56708291+boygl34@users.noreply.github.com>
2023-06-14 10:07:16 +0700

    add

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,11 +19,11 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/Paint' ? 'page' : undefined}>
+				<a href="/Paint">Paint</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname.startsWith('/Body') ? 'page' : undefined}>
+				<a href="/Body">Sverdle</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
---
+++ src/routes/Paint/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+	import {  Carousel, CarouselControl, CarouselItem,Image,FormGroup,Input,Label } from 'sveltestrap';
+	const Sach=[{Ten:"TPR100V",Trang:32},
+				{Ten:"TPR101V",Trang:42},
+				{Ten:"TPR102V",Trang:22},
+				{Ten:"TPR103V",Trang:30},
+				{Ten:"TPR104V",Trang:38},
+				{Ten:"TPR105V",Trang:34},
+				{Ten:"TPR106V",Trang:58},
+				{Ten:"TPR107V",Trang:26},
+				{Ten:"TPR108V",Trang:22}]
+    let items=[]
+	let SoTrang 
+	let SachHoc='TPR100V'
+	let activeIndex = 0;
+	getLink()
+	function getLink() {
+		console.log(SachHoc);
+        activeIndex = 0
+		items=[];
+		SoTrang=42
+	for (let SoTrang = 1; SoTrang <= 42; SoTrang++) {
+	if(SoTrang<10){SoTrang = "0"+SoTrang}else{SoTrang=SoTrang}
+	items.push({url:`https://flame-lowly-cirrus.glitch.me/Paint/${SachHoc}/${SoTrang}.jpg`})
+	}
+	console.log(items);
+	}
+
+	
+
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+<FormGroup>
+	<Input type="select" name="select" bind:value={SachHoc} on:change={getLink}>
+		{#each Sach as ten}
+			<option value={ten.Ten}>{ten.Ten}</option>
+		{/each}
+	</Input>
+</FormGroup>
+
+<Carousel bind:items={items} bind:activeIndex>
+	<div class="carousel-inner">
+		{#each items as item, index}
+			<CarouselItem bind:activeIndex itemIndex={index}>
+				<Image fluid src={item.url} />
+			</CarouselItem>
+		{/each}
+	</div>
+
+	<CarouselControl direction="prev" bind:activeIndex {items} />
+	<CarouselControl direction="next" bind:activeIndex {items} />
+</Carousel>
